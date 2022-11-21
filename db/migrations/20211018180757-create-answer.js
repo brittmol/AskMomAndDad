eee9a32d8569044087b573660b6c72bef99e076a
Brittany Moliver <britt.moliver@gmail.com>
2022-11-20 22:37:09 -0500

    render set up

--- db/migrations/20211018180757-create-answer.js
+++ db/migrations/20211018180757-create-answer.js
@@ -1,38 +1,46 @@
-'use strict';
+"use strict";
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Answers', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+    return queryInterface.createTable(
+      "Answers",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        content: {
+          allowNull: false,
+          type: Sequelize.TEXT,
+        },
+        questionId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          references: { model: "Questions" },
+        },
+        userId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          references: { model: "Users" },
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      content: {
-        allowNull: false,
-        type: Sequelize.TEXT
-      },
-      questionId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: 'Questions' }
-      },
-      userId: {
-        allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: 'Users' }
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+      options
+    );
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Answers');
-  }
+    return queryInterface.dropTable("Answers", options);
+  },
 };
--- db/seeders/20211020210109-userSeeder.js
+++ db/seeders/20211020210109-userSeeder.js
@@ -1,75 +1,76 @@
-'use strict';
+"use strict";
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 // const faker = require('faker');
-const bcrypt = require('bcryptjs');
-
+const bcrypt = require("bcryptjs");
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-
-    return queryInterface.bulkInsert('Users', [
+    options.tableName = "Users";
+    return queryInterface.bulkInsert(options, [
       {
-        username: 'John',
-        email: 'john@john.com',
-        hashedPassword: bcrypt.hashSync('password'),
+        username: "John",
+        email: "john@john.com",
+        hashedPassword: bcrypt.hashSync("password"),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        username: 'Tammy',
-        email: 'tammy@tammy.com',
-        hashedPassword: bcrypt.hashSync('password'),
+        username: "Tammy",
+        email: "tammy@tammy.com",
+        hashedPassword: bcrypt.hashSync("password"),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        username: 'Phil',
-        email: 'phil@phil.com',
-        hashedPassword: bcrypt.hashSync('password'),
+        username: "Phil",
+        email: "phil@phil.com",
+        hashedPassword: bcrypt.hashSync("password"),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        username: 'Sarah',
-        email: 'sarah@sarah.com',
-        hashedPassword: bcrypt.hashSync('password'),
+        username: "Sarah",
+        email: "sarah@sarah.com",
+        hashedPassword: bcrypt.hashSync("password"),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        username: 'Todd',
-        email: 'todd@todd.com',
-        hashedPassword: bcrypt.hashSync('password'),
+        username: "Todd",
+        email: "todd@todd.com",
+        hashedPassword: bcrypt.hashSync("password"),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        username: 'Orlando',
-        email: 'orlando@orlando.com',
-        hashedPassword: bcrypt.hashSync('password'),
+        username: "Orlando",
+        email: "orlando@orlando.com",
+        hashedPassword: bcrypt.hashSync("password"),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        username: 'Alexis',
-        email: 'alexis@alexis.com',
-        hashedPassword: bcrypt.hashSync('password'),
+        username: "Alexis",
+        email: "alexis@alexis.com",
+        hashedPassword: bcrypt.hashSync("password"),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
       {
-        username: 'demo',
-        email: 'demo@demo.com',
-        hashedPassword: bcrypt.hashSync('Demo!1'),
+        username: "demo",
+        email: "demo@demo.com",
+        hashedPassword: bcrypt.hashSync("Demo!1"),
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
       },
-    ], {});
-
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
-
-    return queryInterface.bulkDelete('Users', null, {});
-
-  }
+    options.tableName = "Users";
+    return queryInterface.bulkDelete(options);
+  },
 };
